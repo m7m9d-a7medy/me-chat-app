1473dd87a6279b35782b99b0d933124e068f2be2
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-01-09 05:04:06 +0200

    login component added

--- src/login/login.js
+++ src/login/login.js
@@ -1,9 +1,94 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import WithStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import styles from './styles';
+import withStyles from '@material-ui/core/styles/withStyles';
+const firebase = require('firebase');
+
 
 class LoginComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: null,
+            password: null,
+            loginError: ''
+        };
+    }
+
+    submitLogin = (e) => {
+        e.preventDefault();
+        
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => {
+                this.props.history.push('/dashboard')
+            })
+            .catch(authError => {
+                console.log(authError);
+                this.setState({
+                    loginError: authError.message
+                });
+            });
+    }
+
+    userTyping = (type, e) => {
+        switch (type) {
+            case 'email':
+                this.setState({
+                    email: e.target.value
+                });
+                break;
+            case 'password':
+                this.setState({
+                    password: e.target.value
+                });
+                break;
+            default:
+                break;
+        }
+    }
     render() {
-        return (<div>Hello From Login Component</div>)
+        const { classes } = this.props;
+        return (
+            <main className={classes.main}>
+                <CssBaseline></CssBaseline>
+                <Paper className={classes.paper}>
+                    <Typography component='h1' variant='h5'>Login</Typography>
+                    <form className={classes.form} onSubmit={(e) => this.submitLogin(e)}>
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor='login-email-input'>Enter Your Email</InputLabel>
+                            <Input autoComplete='email' autoFocus id='login-email-input' onChange={(e) => this.userTyping('email', e)}></Input>
+                        </FormControl>
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor='login-password-input'>Enter Your Password</InputLabel>
+                            <Input autoFocus id='login-password-input' onChange={(e) => this.userTyping('password', e)} type='password'></Input>
+                        </FormControl>
+                        <Button type='submit' fullWidth variant='contained' className={classes.submit} color='primary'>Log In</Button>
+                    </form>
+                    {
+                        this.state.loginError ?
+                    <Typography component='h5' variant='h6' className={classes.errorText}>
+                        Incorrect Login Information
+                        <br></br>
+                        {this.state.loginError}
+                    </Typography> :
+                            null
+                    }
+                    <Typography component='h5' variant='h6' className={classes.noAccountHeader}>Don't Have An Account?</Typography>
+                    <Link className={classes.signUpLink} to='/signup'>Sign Up!</Link>
+                </Paper>
+            </main>
+        );
     }
 }
 
-export default LoginComponent;
+export default withStyles(styles)(LoginComponent);
