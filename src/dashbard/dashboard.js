fc5d15bff5d987567f949b1703c5165618dccbb2
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-01-09 06:21:26 +0200

    chat list component added

--- src/dashbard/dashboard.js
+++ src/dashbard/dashboard.js
@@ -1,8 +1,67 @@
 import React from 'react';
+import ChatListComponent from '../chatList/chatList';
+const firebase = require('firebase');
 
 class DashboardComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedChat: null,
+            newChatFormVisible: false,
+            email: null,
+            chats: []
+        };
+    }
+
+    newChatBtnClicked = () => {
+        this.setState({
+            newChatFormVisible: true,
+            selectedChat: null
+        });
+    }
+
+    selectChat = (chatIndex) => {
+        console.log(chatIndex);
+    }
+
+    componentDidMount(){
+        firebase
+            .auth()
+            .onAuthStateChanged(async _user => {
+                if(!_user){
+                    this.props.history.push('/login');
+                }
+                else {
+                    await firebase
+                        .firestore()
+                        .collection('chats')
+                        .where('users', 'array-contains', _user.email)
+                        .onSnapshot(async result => {
+                            const chats = result.docs.map(_document => {
+                                return _document.data();
+                            });
+                            await this.setState({   
+                                email: _user.email,
+                                chats: chats
+                            });
+                            console.log(this.state);
+                        });
+                }
+            })
+    }
+
     render() {
-        return (<div>Hello From Dashboard Component</div>)
+        return (
+            <ChatListComponent
+                history={this.props.history}
+                newChatFn={this.newChatBtnClicked}
+                selectChatFn={this.selectChat}
+                chats={this.state.chats}
+                userEmail={this.state.email}
+                selectedChatIndex={this.state.selectedChat}
+            >
+            </ChatListComponent>
+        )
     }
 }
 
